4417d530f57c1c40d5e232a2b1dafc4f1de211b8
Kris Sankaran <sankaran.kris@gmail.com>
2017-06-03 15:52:34 -0700

    revisions using data extracted from phylo

--- june2/hclust.js
+++ june2/hclust.js
@@ -1,7 +1,7 @@
 
 var root = d3.stratify()
     .id(function(d) { return d.child; })
-    .parentId(function(d) { return d.parent; })(tree);
+    .parentId(function(d) { return d.id; })(tree);
 
 function depths(root) {
   if (root.parent === null) {
@@ -19,40 +19,52 @@ function depths(root) {
   }
 }
 
+var elem_height = 700;
+var elem_width = 1000;
 var elem = d3.select("#vis")
     .append("svg")
     .attrs({
-      "width": 1000,
-      "height": 1000
+      "width": elem_width,
+      "height": elem_height
     });
 
 elem.append("rect")
   .attrs({
     "fill": "#F8F8F8",
-    "width": 1000,
-    "height": 1000
+    "width": elem_width,
+    "height": elem_height
   });
 
 d3.cluster()(root);
-depths(root);
-
-var ids = root.descendants().map(function(d) { return d.id; });
-var leaf_depths = root.leaves().map(function(d) { return d.data.depth; });
+var coords = {
+  "x": root.descendants().map(function(d) { return d.data.x; }),
+  "y": root.descendants().map(function(d) { return d.data.y; })
+};
 
 var scales = {
-  "x": d3.scaleBand()
-    .domain(ids)
-    .range([0, 1000]),
-  "y": d3.scaleLinear()
-    .domain([0, d3.max(leaf_depths)])
-    .range([0, 1000])
+  "x": d3.scaleLinear()
+    .domain(d3.extent(coords.x))
+    .range([0, elem_width]),
+  "tree_y": d3.scaleLinear()
+    .domain(d3.extent(coords.y))
+    .range([elem_height / 2, 0])
 };
 
 elem.selectAll(".node")
   .data(root.descendants()).enter()
   .append("circle")
   .attrs({
-    "cx": function(d) { return scales.x(d.id); },
-    "cy": function(d) { return scales.y(d.data.depth); },
-    "r": 5
+    "class": "node",
+    "cx": function(d) { return scales.x(d.data.x); },
+    "cy": function(d) { return scales.tree_y(d.data.y); }
+  });
+
+elem.selectAll(".link")
+  .data(root.links())
+  .enter().append("path")
+  .attrs({
+    "class": "link",
+    "d": d3.linkVertical()
+      .x(function(d) { return scales.x(d.data.x); })
+      .y(function(d) { return scales.tree_y(d.data.y); })
   });
