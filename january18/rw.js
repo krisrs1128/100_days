9d654c94a5b9f68c7e96faf491610dce741c4845
Kris Sankaran <sankaran.kris@gmail.com>
2018-01-18 13:54:03 -0800

    basic random walk

--- january18/rw.js
+++ january18/rw.js
@@ -6,14 +6,15 @@
 
 // create margins using margin convention
 // https://bl.ocks.org/mbostock/3019563
-var margin = {top: 20, right: 10, bottom: 20, left: 10};
+var margin = {top: 0, right: 0, bottom: 0, left: 0};
 var opts = {
   "elem_width": 200,
   "elem_height": 200,
-  "delta": 1,
+  "delta": 5,
   "radius": 2
-}
+};
 
+// Create background rectangle
 var svg_elem = d3.select("#vis")
     .append("svg")
     .attrs({
@@ -31,3 +32,47 @@ var elem = svg_elem.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 opts.elem_width = opts.elem_width - margin.right - margin.left;
 opts.elem_height = opts.elem_height - margin.top - margin.bottom;
+
+d3.timer(animate);
+
+circles = [
+  {
+    "index": 0,
+    "x": opts.elem_width / 2,
+    "y": opts.elem_height / 2
+  }
+];
+
+function add_modulo(x, h) {
+  var proposal = x;
+  if (x < 0) {
+    proposal += h;
+  } else if (x > h) {
+    proposal %= h;
+  }
+  return proposal;
+
+}
+
+function animate(elapsed) {
+  var last_circle = circles[circles.length - 1];
+  circles.push({
+    "index": last_circle.index + 1,
+    "r": opts.radius,
+    "x": last_circle.x + (Math.random() - 0.5) * opts.delta,
+    "y": last_circle.y + (Math.random() - 0.5) * opts.delta
+  });
+
+  var circles_elem = svg_elem.selectAll("circle")
+      .data(circles);
+
+  circles_elem.enter()
+    .append("circle");
+
+  circles_elem.attrs({
+    "cx": function(d) { return add_modulo(d.x, opts.elem_width); },
+    "cy": function(d) { return add_modulo(d.y, opts.elem_height); },
+    "fill": "black",
+    "r": function(d) { return d.r; }
+  });
+}
